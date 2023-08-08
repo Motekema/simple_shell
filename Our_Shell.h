3669a1239acf7fab6495e0ef7218996a8e90e6e6
Motekema <motekema@gmai.com>
2023-08-08 00:17:42 -0700

    My commit

--- Our_Shell.h
+++ Our_Shell.h
@@ -1,7 +1,10 @@
 #ifndef _OUR_SHELL_H_
 #define _OUR_SHELL_H_
 
-
+int main(void);
+int main(int ac, char **av);
+int main(int ac, char **av, char **env);
+int main(int ac, char **av);
 char *_getenv(const char *name);
 int _setenv(const char *name, const char *value, int overwrite);
 int _unsetenv(const char *name);
