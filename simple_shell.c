7723a38becffdd723b70af7e29625238664de6de
Motekema <motekema@gmai.com>
2023-08-11 03:10:47 -0700

    My commit

--- simple_shell.c
+++ simple_shell.c
@@ -1,42 +1,42 @@
 #include "Our_Shell.h"
-
 #define MAX_INPUT_LENGTH 100
-
-int main(void) {
+int main(void)
+{
 char input[MAX_INPUT_LENGTH];
 char *args[MAX_INPUT_LENGTH];
 char *token;
 pid_t pid;
 int status;
 int argc = 0;
-
-while (1) {
-char prompt[] = "#Motekema&Joshua$ ";
+while (1)
+{
 write(STDOUT_FILENO, prompt, sizeof(prompt) - 1);
-
-if (fgets(input, sizeof(input), stdin) == NULL) {
+if (fgets(input, sizeof(input), stdin) == NULL)
+{
 break;
 }
-
 input[strcspn(input, "\n")] = '\0';
-
 token = strtok(input, " ");
-while (token != NULL) {
+while (token != NULL)
+{
 args[argc++] = token;
 token = strtok(NULL, " ");
 }
 args[argc] = NULL;
-
-if ((pid = fork()) == 0) {
+((pid = fork()) == 0)
+{
 execvp(args[0], args);
 perror("Error of execvp");
 exit(1);
-} else if (pid < 0) {
+}
+else if (pid < 0)
+{
 perror("Error of forking");
-} else {
+}
+else
+{
 waitpid(pid, &status, 0);
 }
 }
 return (0);
 }
-
