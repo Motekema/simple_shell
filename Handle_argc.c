efa4ace614ef5e90720383c789c5209a3f2de59d
Motekema <motekema@gmai.com>
2023-08-15 04:08:30 -0700

    My commit

--- Handle_argc.c
+++ Handle_argc.c
@@ -3,7 +3,11 @@
 /**
  * parse_input - Parses the input string into arguments.
  *
- * @param input The input string to parse.
+ * This function takes an input string.
+ * using space (' ') as a delimiter. The arguments are stored in the 'args'
+ * array, and the number of arguments is updated in the 'argc' variable.
+ *
+ * @param input The input string to be parsed.
  * @param args An array to store the parsed arguments.
  * @param argc A pointer to an integer to store the number of arguments.
  */
@@ -23,7 +27,12 @@ args[*argc] = NULL;
 /**
  * execute_command - Executes a command with the provided arguments.
  *
- * @param args An array of arguments, where the first argument is the command to execute.
+ * This function attempts to execute a command using the 'execvp' system call.
+ * The 'args' array should contain the command to be executed as first element,
+ * followed by its arguments. If the execution fails, an error
+ * and the program exits with a failure status.
+ *
+ * @param args An array of arguments, first argument is the command to execute.
  */
 
 void execute_coommand(char **args)
