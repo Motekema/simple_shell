314e8ef87bfea11b7dd841a69aaa74ad611f05cd
Motekema <motekema@gmai.com>
2023-08-16 03:17:30 -0700

    My commit

---
+++ exit.c
@@ -0,0 +1,29 @@
+#include "Our_Shell.h"
+
+#define MAX_INPUT_LENGTH 1024
+
+int main(void)
+{
+char input[MAX_INPUT_LENGTH];
+
+while (1)
+{
+write(1, "#Motekema&Josua$ ", 13);
+read(0, input, sizeof(input));
+
+/* Remove the newline character from input */
+input[strcspn(input, "\n")] = '\0';
+
+/* Check if the input is the exit commant */
+if (strcmp(input, "exit") == 0)
+{
+write(1, "Exiting the shell...\n", 21);
+break;
+}
+char unknown[] = "Unknown command: ";
+write(1, unknown, sizeof(unknown));
+write(1, input, strlen(input));
+write(1, "\n", 1);
+}
+return (0);
+}
