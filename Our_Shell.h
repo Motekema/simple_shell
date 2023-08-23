58095db747272cc1fbcc479029dbfe9583b02c65
Lebohang1821 <millboylebohang@gmail.com>
2023-08-23 13:13:56 -0700

    Simple_shell

--- Our_Shell.h
+++ Our_Shell.h
@@ -156,7 +156,7 @@ int _putfd(char c, int fd);
 int _putsfd(char *str, int fd);
 
 /* toem_string.c */
-int _strlen(char *);
+int _strgen(char *);
 int _strcmp(char *, char *);
 char *starts_with(const char *, const char *);
 char *_strcat(char *, char *);
