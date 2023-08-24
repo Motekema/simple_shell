bfc06c2fbbc8027f509b89163fc3488e0a263c5a
Lebohang1821 <millboylebohang@gmail.com>
2023-08-24 14:15:07 -0700

    simple_shell

--- realloc.c
+++ realloc.c
@@ -50,7 +50,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (new_size == old_size)
 		return (ptr);
 
-	p = malloc(new_size);
+	l = malloc(new_size);
 	if (!l)
 		return (NULL);
 
